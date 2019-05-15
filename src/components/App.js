27842d28027c505bf822ef1aeff605e561020c65
johnpospisil <audiofreak7@gmail.com>
2019-05-15 12:32:57 -0700

    Access data from the SearchBar from the App. Invoking Callbacks in Children.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-// a functional component
-const App = () => {
-  return (
-    <div className="ui container" style={{marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  );
-};
+// a class-based component
+class App extends React.Component {
+
+onSearchSubmit(term) {
+  console.log(term);
+}
+
+  render() {
+    return (
+      <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,19 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  // event handler
+  // using an arrow function below ensures that 'this' refers to the instance of the SearchBar, instead of the App.
+  onFormSubmit = event => {
+    event.preventDefault(); // prevent the form from clearing when the user presses ENTER
+    this.props.onSubmit(this.state.term);
+  }
+
+  
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
           <label>Image Search</label>
           {/* Abbreviated event handler */}
