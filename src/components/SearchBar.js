efc3353603e5c07193d638b0e218dabeb81090b0
johnpospisil <audiofreak7@gmail.com>
2019-05-15 10:32:20 -0700

    An example of abbreviated event handler syntax

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,12 @@ import React from 'react';
 
 // class-based component
 class SearchBar extends React.Component {
-
+  
   onInputChange(event) {
     // event.target.value shows what has been typed into the input
     console.log(event.target.value);
   }
+
 
   render() {
     return (
@@ -15,10 +16,11 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
           <label>Image Search</label>
-          {/* Call the onInputChange function whenever the following input is changed. */}
-          {/* notice there is no '()' after 'onInputChange'.*/} 
-          {/* This is so when a function is used with onChange. */}
+          {/* Abbreviated event handler syntax can be seen in the comment below. */}
+          {/* In this case, the onInputChange function could be deleted since it is not needed. */}
+          {/* <input type="text" onChange={e => console.log(e.target.value)} /> */}
             <input type="text" onChange={this.onInputChange} />
+            
           </div>
         </form>
       </div>
