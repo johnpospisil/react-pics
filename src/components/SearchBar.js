2bc7bd5621098346053e767aba94de71bd80ce88
johnpospisil <audiofreak7@gmail.com>
2019-05-15 10:24:49 -0700

    Create an event handler for when the text in the SearchBar is updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,22 @@ import React from 'react';
 
 // class-based component
 class SearchBar extends React.Component {
+
+  onInputChange(event) {
+    // event.target.value shows what has been typed into the input
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
           <label>Image Search</label>
-            <input type="text" />
+          {/* Call the onInputChange function whenever the following input is changed. */}
+          {/* notice there is no '()' after 'onInputChange'.*/} 
+          {/* This is so when a function is used with onChange. */}
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
