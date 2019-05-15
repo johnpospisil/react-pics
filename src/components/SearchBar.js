130bdcea15b420c44b8a1eea7ccc605bcab94fac
johnpospisil <audiofreak7@gmail.com>
2019-05-15 10:05:43 -0700

    Create the SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+// class-based component
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
