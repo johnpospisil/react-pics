7ae761ba7e77a085e286b3da346631f6f56a3d7b
johnpospisil <audiofreak7@gmail.com>
2019-05-16 11:32:38 -0700

    Map through the array of pic urls, make 'img' elements

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const ImageList = props => {
-  console.log(props.images);
+  // make an array of 'img' elements with the pic urls in the 'src' attribute
+  const images = props.images.map((image) => {
+    // console.log(image.alt_description);
+    return <img alt={image.alt_description} src={image.urls.regular} />
+  });
   
-  return <div>ImageList</div>
+  return <div>{images}</div>
 };
 
 export default ImageList;
