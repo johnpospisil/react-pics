d62500efb207a8893577647c700b10a8282aafe5
johnpospisil <audiofreak7@gmail.com>
2019-05-18 09:14:57 -0700

    Get image height, then set vertical spacing between images using 10px spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10 + 1);
+
+    this.setState({ spans });
+  }
+
   render() {
     const { alt_description, urls } = this.props.image;
     // console.log(alt_description);
     // console.log(urls);
 
     return (
-      <div>
-        <img alt={alt_description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={alt_description} src={urls.regular} />
       </div>
     )
   }
